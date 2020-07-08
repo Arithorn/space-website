4aa143b40a513bd21aeee35a58a2ef3ee99a5dd6
Peet Gouws <peet@gouws.com>
2020-07-08 10:36:45 +0200

    travis stuff

--- src/components/launchlist.js
+++ src/components/launchlist.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button } from "reactstrap";
+// import { Button } from "reactstrap";
 
 import MainContext from "../contexts/main-context";
 import LaunchCard from "./launchcard";
