c9e5d4fad582d3246afbfe603e0ad269eb7432e9
Peet Gouws <peet@gouws.com>
2020-07-08 14:11:48 +0200

    Adding Flags

--- src/components/launchcard.js
+++ src/components/launchcard.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { Card, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
+import {
+  Card,
+  CardText,
+  CardBody,
+  CardTitle,
+  CardSubtitle,
+  Media,
+} from "reactstrap";
 import Countdown from "react-countdown";
 
 class LaunchCard extends React.Component {
@@ -8,14 +15,34 @@ class LaunchCard extends React.Component {
     return (
       <div>
         <Card>
-          <CardBody>
-            <CardTitle>Launch Name : {this.props.name}</CardTitle>
-            <CardSubtitle>Rocket Name : {this.props.rocketname}</CardSubtitle>
-            <CardText>
-              Launch Window : {this.props.startwindow} (
-              <Countdown date={new Date(this.props.startwindow)}></Countdown>)
-            </CardText>
-          </CardBody>
+          <div className="row no-gutters">
+            <div className="col-auto">
+              <Media left middle>
+                <Media
+                  object
+                  left
+                  middle
+                  src="https://www.countryflags.io/za/shiny/64.png"
+                  alt=""
+                />
+              </Media>
+            </div>
+            <div className="col">
+              <CardBody>
+                <CardTitle>Launch Name : {this.props.name}</CardTitle>
+                <CardSubtitle>
+                  Rocket Name : {this.props.rocketname}
+                </CardSubtitle>
+                <CardText>
+                  Launch Window : {this.props.startwindow} (
+                  <Countdown
+                    date={new Date(this.props.startwindow)}
+                  ></Countdown>
+                  )
+                </CardText>
+              </CardBody>
+            </div>
+          </div>
         </Card>
       </div>
     );
